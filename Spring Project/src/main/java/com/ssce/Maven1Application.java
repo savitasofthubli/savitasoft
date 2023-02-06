2469872a6c26715c51d5a28212c6a4aea2e3478a
sumitmeharwade1@gmail.com <sumitmeharwade1@gmail.com>
2023-02-07 00:37:52 +0530

    Sumit 07-02-23

--- Spring Project/src/main/java/com/ssce/Maven1Application.java
+++ Spring Project/src/main/java/com/ssce/Maven1Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SavitaSoftApplication {
+public class Maven1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SavitaSoftApplication.class, args);
+		SpringApplication.run(Maven1Application.class, args);
 	}
 
 }
