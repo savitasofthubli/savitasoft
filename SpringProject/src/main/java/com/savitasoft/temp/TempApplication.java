be1f8ca3f02813e19473182c63bcf1d40ac04bcf
sumitmeharwade1@gmail.com <sumitmeharwade1@gmail.com>
2023-04-08 16:56:51 +0530

    Sumit 08-04-05

--- SpringProject/src/main/java/com/savitasoft/temp/TempApplication.java
+++ SpringProject/src/main/java/com/savitasoft/temp/TempApplication.java
@@ -1,10 +1,23 @@
 package com.savitasoft.temp;
 
+import com.savitasoft.temp.otpConfig.TwilioConfig;
+import com.twilio.Twilio;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TempApplication {
+
+	@Autowired
+	private TwilioConfig twilioConfig;
+
+	@PostConstruct
+	public void initTwilio()
+	{
+		Twilio.init(twilioConfig.getAccountSid(), twilioConfig.getAuthToken());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TempApplication.class, args);
