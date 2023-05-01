8ba00b3b6a8e09174a811040e2ecbbe8e9b4a72b
Varun033 <rohinrvpatil@gmail.com>
2023-05-01 14:28:14 +0530

    Updated New Project 1-05-23

--- SpringProject/src/main/java/com/savitasoft/temp/Repository/TrainerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.savitasoft.temp.Repository;
-
-import com.savitasoft.temp.model.Trainer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TrainerRepository extends JpaRepository<Trainer,Long> {
-}
--- SpringProject/src/main/java/com/savitasoft/temp/registration/repository/CourseRegistrationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.savitasoft.temp.registration.repository;
-
-import com.savitasoft.temp.registration.model.CourseRegistration;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRegistrationRepository extends JpaRepository<CourseRegistration,Long> {
-}
