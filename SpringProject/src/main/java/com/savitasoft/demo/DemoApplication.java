5d57442e53c717e74140d98665285c8763aa4306
Varun033 <rohinrvpatil@gmail.com>
2023-05-01 16:48:30 +0530

    Updated New Project 1-05-23

--- SpringProject/src/main/java/com/savitasoft/demo/DemoApplication.java
+++ SpringProject/src/main/java/com/savitasoft/demo/DemoApplication.java
@@ -1,10 +1,23 @@
 package com.savitasoft.demo;
 
+import com.savitasoft.demo.otpConfig.TwilioConfig;
+import com.twilio.Twilio;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+
+	@Autowired
+	private TwilioConfig twilioConfig;
+
+	@PostConstruct
+	public void initTwilio()
+	{
+		Twilio.init(twilioConfig.getAccountSid(), twilioConfig.getAuthToken());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
