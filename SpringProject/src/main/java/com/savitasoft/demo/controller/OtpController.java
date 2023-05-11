9eef7d08f3c46b17ab4b911e89aefa930e962212
Varun033 <rohinrvpatil@gmail.com>
2023-05-11 08:41:28 +0530

    Updated on 11-05-23 OTP from backend is disabled any otp can be used to access database!

--- SpringProject/src/main/java/com/savitasoft/demo/controller/OtpController.java
+++ SpringProject/src/main/java/com/savitasoft/demo/controller/OtpController.java
@@ -5,6 +5,7 @@ import com.savitasoft.demo.otpDto.PasswordResetRequest;
 import com.savitasoft.demo.otpDto.PasswordResetResponse;
 import com.savitasoft.demo.otpService.TwilioOtpService;
 import com.savitasoft.demo.registration.model.Registration;
+import com.savitasoft.demo.registration.service.RegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,13 +17,16 @@ public class OtpController {
 	@Autowired
 	TwilioOtpService twilioOtpService;
 
+	@Autowired
+	RegistrationService registrationService;
 	@PostMapping("/send")
 	public ResponseEntity<PasswordResetResponse> SendOTP(@RequestBody PasswordResetRequest passwordResetRequest) {
-		return twilioOtpService.sendOTPForPasswordReset(passwordResetRequest);
+		PasswordResetResponse pss=new PasswordResetResponse("DElIVERED","OTP IS 12345");
+		return ResponseEntity.ok(pss);
 	}
 
 	@PostMapping("/validate")
 	public ResponseEntity<Registration> ValidateOTP(@RequestBody PasswordResetRequest passwordResetRequest) {
-		return twilioOtpService.validateOTP(passwordResetRequest.getOtp(), passwordResetRequest.getPhonenumber());
+		return ResponseEntity.ok(registrationService.findByPhoneNumber(passwordResetRequest.getPhonenumber()));
 	}
 }
